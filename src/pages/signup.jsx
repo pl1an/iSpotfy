ff2284f304828f9cf5c8d9a0dc6784ba923adb69
Rodrigo Pierre <rodrigopierre05@gmail.com>
2024-06-11 09:24:14 -0300

    continuei o css da login-page e fiz a signup-page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './styles/signup.css'
 
 export function Signup(){
-    return(
-        <Link to={"/login"}>Login</Link>
-    )
+    return (
+        <div className="signupclass">
+            <div className="signup-tittle">
+                <h1>Inscrever-se em uma conta grátis do iSpotify ®</h1>
+            </div>
+            <form>
+                <input placeholder="Email"></input>
+                <input placeholder="Crie uma senha"></input>
+                <input placeholder="Como devemos chamar você ?"></input>
+                <button type="submit">Cadastrar</button>
+            </form>
+            <div className="login_link">
+                <p>Já é um usuário do iSpotify ? </p>
+                <Link to={"/login"} className="linking_login">Faça login</Link>
+            </div>
+        </div>
+     )
+    
 }
