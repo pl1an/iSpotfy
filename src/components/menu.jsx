a9b01051e2e0923713e74ab148d72dac9e850f56
BrunoSilva14 <brunosoaresbd@hotmail.com>
2024-06-12 22:56:08 -0300

    Adicao dos icones no menu

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import "../components/menu.css"
-import { Link } from 'react-router-dom'
+import React from 'react';
+import "../components/menu.css";
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
   return (
     <div className='menu'>
-        <div className='menu-title'>
-            <h1>iSpotify</h1>
-            <p>®</p>
-        </div>
-        <Link to={"/artists"} className='menu-btn'>Artistas</Link>
-        <Link to={"/likedmusics"} className='menu-btn'>Músicas Curtidas</Link>
-        <Link to={"/account"} className='menu-btn'>Minha Conta</Link>
-        <div className='logout'>
-          <Link to={"/login"} className='logout-btn'>Logout</Link>
-        </div>
+      <div className='menu-title'>
+        <h1>iSpotify</h1>
+        <p>®</p>
+      </div>
+      <Link to={"/artists"} className='menu-btn'>
+        <span className="material-icons icon">trip_origin</span> Artistas
+      </Link>
+      <Link to={"/likedmusics"} className='menu-btn'>
+        <span className="material-icons icon">favorite</span> Músicas Curtidas
+      </Link>
+      <Link to={"/account"} className='menu-btn'>
+        <span className="material-icons icon">account_circle</span> Minha Conta
+      </Link>
+      <div className='logout'>
+        <Link to={"/login"} className='logout-btn'>
+          <span className="material-icons icon">logout</span> Logout
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
