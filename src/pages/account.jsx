f7447c45da1eeb0d872a991610a269211d7744eb
Rodrigo Pierre <rodrigopierre05@gmail.com>
2024-06-12 22:50:02 -0300

    página minha conta

--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -7,7 +7,17 @@ const Account = () => {
     <div className='account'>
         <Menu />
         <div className='account-right'>
-            Minha Conta
+            <h1>Minha Conta</h1>
+            <div className="input-container">
+              <h2 className='name-account'>Nome</h2>
+              <span className="material-icons icon">account_circle</span>
+            </div>
+            <div className="input-container">
+                <h2 className='email-account'>Email</h2>
+                <span className="material-icons icon">mail</span>
+            </div>
+            <button type="submit">Trocar E-mail</button>
+            <button type="submit">Trocar Senha</button>
         </div>
     </div>
   )
