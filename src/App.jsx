add5ce7e1a874933bc6953c1dcd9ee650cf6fff9
rafaelmagesty <rafael.am1@hotmail.com>
2024-06-14 12:26:36 -0300

    welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <Link to='/login'>link temporário para o design da página de login</Link>
+    <div className='welcome-class'>
+      <div className="tittle">
+            <h1>iSpotify</h1>
+            <p>®</p>  
+        </div>
+      <div className="container">
+        <h1>Bem-vindo ao iSpotify</h1>
+        <p>Sua experiência musical personalizada começa aqui. Descubra novas músicas, artistas e playlists só para você.</p>
+        <Link to="/login" className="btn">Começar</Link>
+    </div>
     </div>
   )
 }
