328da04e911ca2eef7839e9f3ce7a3bd81fa12f2
Rodrigo Pierre <rodrigopierre05@gmail.com>
2024-06-15 20:11:12 -0300

    welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import './App.css'
 function App() {
   return (
     <div className='welcome-class'>
-      <div className="tittle">
+      <div className="welcome-left">
+        <div className='welcome-title'>
             <h1>iSpotify</h1>
-            <p>®</p>  
+            <p>®</p> 
         </div>
-      <div className="container">
-        <h1>Bem-vindo ao iSpotify</h1>
+        <div className='welcome-img'>
+            <img src="\src\assets\img-welcome.png" alt="img" className='welcome-image'/>
+        </div>
+      </div>
+      <div className="welcome-right">
         <p>Sua experiência musical personalizada começa aqui. Descubra novas músicas, artistas e playlists só para você.</p>
-        <Link to="/login" className="btn">Começar</Link>
-    </div>
+        <Link to={"/login"} className="welcome-btn">Faça login</Link>
+        <Link to={"/signup"} className="welcome-btn">Cadastre-se</Link>
+      </div>
     </div>
   )
 }
