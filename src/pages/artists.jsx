a294a84a96afaa62c4307d8b3405782ef0c2cd57
rafaelmagesty <rafael.am1@hotmail.com>
2024-06-28 12:47:36 -0300

    tentativa de retornar os artistas na pagina artists

--- src/pages/artists.jsx
+++ src/pages/artists.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState }  from 'react'
 import Menu from '../components/menu'
 import "./styles/artists.css"
 import ArtistContainer from '../components/artist-container'
+import api from '../api'
 
 const Artists = () => {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    api.get('/api/artists')
+      .then(response => {
+        setArtists(response.data);
+      })
+      .catch(error => {
+        console.log('Erro ao buscar artistas', error);
+        alert('Erro ao buscar artistas');
+      });
+  }, []);
+  
+  
   return (
     <div className='artists'>
         <Menu />
         <div className='artists-right'>
             <h1>Artistas</h1>
             <div className='artists-container'>
-              <ArtistContainer name="cantor 1" img="src\assets\react.svg" id="cantor1"/>
-              <ArtistContainer name="cantor 2" img="src\assets\react.svg" id="cantor2"/>
-              <ArtistContainer name="cantor 3" img="src\assets\react.svg" id="cantor3"/>
-              <ArtistContainer name="cantor 4" img="src\assets\react.svg" id="cantor4"/>
-              <ArtistContainer name="cantor 5" img="src\assets\react.svg" id="cantor5"/>
-            </div>
-            <div className='artists-container'>
-              <ArtistContainer name="cantor 6" img="src\assets\react.svg" id="cantor6"/>
-              <ArtistContainer name="cantor 7" img="src\assets\react.svg" id="cantor7"/>
-              <ArtistContainer name="cantor 8" img="src\assets\react.svg" id="cantor8"/>
-              <ArtistContainer name="cantor 9" img="src\assets\react.svg" id="cantor9"/>
-              <ArtistContainer name="cantor 10" img="src\assets\react.svg" id="cantor10"/>
+              {artists.map((artist) => (
+                <ArtistContainer
+                  key={artist.id}
+                  name={artist.name}
+                  img={artist.image}
+                  id={artist.id}
+                />
+              ))}
             </div>
         </div>
     </div>
-  )
+  );
 }
 
 export default Artists
