d9b6cde997f8d0704a73b643ce40f09b0e663d7f
Rodrigo Pierre <rodrigopierre05@gmail.com>
2024-06-28 21:26:57 -0300

    adicionei capa do album de cada artista

--- src/pages/artistmusics.jsx
+++ src/pages/artistmusics.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Menu from '../components/menu'
 import Playlist from '../components/playlist'
+import api from '../api'
 
 function Artistmusics() {
     const { id } = useParams();
+    const [img, setImg] = useState();
+
+    useEffect(() => {
+      api.get(`/artists/${id}`)
+        .then(response => {
+          console.log(response.data);
+          setImg(response.data.image);
+        })
+        .catch(error => {
+          console.log('Erro ao buscar artistas', error);
+          alert('Erro ao buscar artistas');
+        });
+    }, []);
+
+
+
   return (
     <div>
         <Menu />
-        <Playlist playlistname={id} localname={id}/>
+        <Playlist playlistname={id} localname={id} img={img}/>
     </div>
   )
 }
